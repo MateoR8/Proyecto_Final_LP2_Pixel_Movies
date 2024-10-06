30ebcdc0b166501b6bae5e0edceb1fece53adfde
Mateo <mateoquispe08@hotmail.com>
2024-10-06 14:17:08 -0500

    Proyecto Base

---
+++ Proyecto_Final_LP2_Pixel_Movies/src/main/java/com/cibertec/proyecto_final_lp2_pixel_movies/ProyectoFinalLp2PixelMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyecto_final_lp2_pixel_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalLp2PixelMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalLp2PixelMoviesApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Final_LP2_Pixel_Movies/src/test/java/com/cibertec/proyecto_final_lp2_pixel_movies/ProyectoFinalLp2PixelMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyecto_final_lp2_pixel_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalLp2PixelMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
